618b12455a887bd6bb335d0eb3bba7169d0c2fca
Marine <levraguemarine@gmail.com>
2021-12-10 17:43:45 +0100

    Mise en place de sass

--- frontend/src/components/Log/index.js
+++ frontend/src/components/Log/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import SignUpForm from './SignUpForm';
-import SignInForm from './SignInForm';
+//import SignInForm from './SignInForm';
 
 const Log = () => {
     const [signUpModal, setSignUpModal] = useState(true);
