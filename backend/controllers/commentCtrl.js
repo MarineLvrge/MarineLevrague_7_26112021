f52da4e40fec4318fb394b26499d8efb100072a2
Marine <levraguemarine@gmail.com>
2022-01-04 18:19:13 +0100

    backend in progress

--- backend/controllers/commentCtrl.js
+++ backend/controllers/commentCtrl.js
@@ -7,17 +7,31 @@ exports.createComment = (req, res, next) => {
         comment: req.body.comment
     })
     .then(() => res.status(201).json({ message: 'Commentaire publié!' }))
-    .catch((error) => res.status(500).json({ message: 'Une erreur est survenue lors de la publication du commentaire' }));
+    .catch((error) => res.status(500).json({ error, message: 'Une erreur est survenue lors de la publication du commentaire' }));
 };
 
 exports.modifyComment = (req, res, next) => {
-
-}
+    Comment.update({
+        comment: req.body.comment
+    }, {
+        where: {
+            id_comment: req.params.id_comment
+        }
+    })
+    .then(() => res.status(200).json({ message: 'Commentaire modifié avec succès!' }))
+    .catch((error) => res.status(500).json({ error, message: 'Une erreur est survenue lors de la modification du commentaire' }));
+};
 
 exports.deleteComment = (req, res, next) => {
-
-}
+    Comment.destroy({
+        where: {
+            id_comment: req.params.id_comment
+        }
+    })
+    .then(() => res.status(200).json({ message: 'Commentaire supprimé avec succès!' }))
+    .catch((error) => res.status(500).json({ error, message: 'Une erreur est survenue lors de la suppression du commentaire!' }));
+};
 
 exports.getAllComments = (req, res, next) => {
-    
+
 }
