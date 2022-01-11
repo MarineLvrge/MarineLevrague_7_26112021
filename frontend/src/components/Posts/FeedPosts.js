2b5394201f41a3f9113b8065a12a07607099fedc
Marine <levraguemarine@gmail.com>
2022-01-11 18:35:07 +0100

    work on get the posts

--- frontend/src/components/Posts/FeedPosts.js
+++ frontend/src/components/Posts/FeedPosts.js
@@ -1,7 +1,6 @@
-import React from "react";
+/*import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const FeedPosts = () => {
 
     let session = false;
 
@@ -14,26 +13,119 @@ const FeedPosts = () => {
     console.log(userId);
     console.log(token);
 
-    axios.get(`${process.env.REACT_APP_URL}api/posts`, {
-        headers: {'authorization' : `Bearer ${token}`}
-    })
-    .then(res => {
-        console.log(res.data);
-    })
-    .catch(err => {'Erreur dans la récupération des posts'});
+
+const FeedPosts = () => {
+    const [posts, setPosts] = useState([]);
 
 
+  
+    useEffect(() => {
+        const fetchPosts = async () => {
+            let res;
+            try {
+                res = await axios.get(`${process.env.REACT_APP_URL}api/posts`, {
+                    headers: {'Authorization' : `Bearer ${token}`,
+                },
+                });
+            } catch(error) {
+                throw error;
+            }
+            console.log(res);
+            setPosts(res.data);
+        }
+        
+        fetchPosts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [`${token}`]);
 
-    if(!session) {
-        window.location = '/connect';
+    console.log(posts);
+
+
+    if(!session){
+        <div>Vous n'êtes pas connecté</div>
     } else {
         return(
             <div className="post-container">
                 <h1>Votre fil d'actualité</h1>
+                {posts.map((item) => (
+                    <div className="postContent" key={item.id_user}>
+                        <div className="postAuthor">{item.id_user ? item.id_user.firstName : 'deleted user'}
+                        </div>
+                        <div className="postText">
+                            <span>{item.title}</span>
+                            <span>{item.content}</span>
+                            <span>{item.attachment}</span>
+                        </div>
+                    </div>
+                ))}
             </div>
         )
     }
+}
 
+
+export default FeedPosts;*/
+
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+    const FeedPosts = () => {
+
+        let session = false;
+
+        if(!sessionStorage.storageToken){
+        window.location = '/connect';
+        } else session = true;
+    
+        const userId = JSON.parse (sessionStorage.storageToken).userId;
+        const token = JSON.parse (sessionStorage.storageToken).token;
+        console.log(userId);
+        console.log(token);
+
+        const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            let res;
+            try {
+                res = await axios.get(`${process.env.REACT_APP_URL}api/posts`, {
+                    headers: {'Authorization' : `Bearer ${token}`,
+                },
+            });
+            } catch(error) {
+                throw error;
+            }
+            console.log(res);
+            setPosts(res.data);
+    }
+            
+        fetchPosts();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [`${token}`]);
+    
+    console.log(posts);
+
+    if(!session){
+        <div>Vous n'êtes pas connecté</div>
+    } else {
+        return(
+            <div className="post-container">
+                <h1>Votre fil d'actualité</h1>
+                {posts.map((item) => (
+                    <div className="postContent" key={item.id_user}>
+                        <div className="postAuthor">{item.id_user ? item.id_user.firstName : 'deleted user'}
+                        </div>
+                        <div className="postText">
+                            <span>{item.title}</span>
+                            <span>{item.content}</span>
+                            <span>{item.attachment}</span>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        )
+    }
 };
 
 export default FeedPosts;
